a77a40df9fd70bd31a2d84d4157cd7d5edc6c87f
SumSumCorn <a01035587180@gmail.com>
2022-02-11 19:16:32 +0900

    mongoDB init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const router = new Router();
+const mongoose = require('mongoose');
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const api = require('./api');
 
@@ -12,6 +25,7 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('listening on port 4000');
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('listening on port %d', port);
 });
